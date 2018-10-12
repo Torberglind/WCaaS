9e6633b0596665b0da757b895a32b2e17d4d48a1
Tor Berglind <berglind.tor@gmail.com>
2018-10-12 18:30:44 +0200

    Script not behaving as intended. Fixed indentation faults

--- tasks.py
+++ tasks.py
@@ -18,22 +18,21 @@ def count():
     tweet_counter = 0
     for file in os.listdir(dirpath):
         filepath = dirpath + "/" + file
-
-    with open (filepath, 'r') as tweet:
-        for line in tweet:
-            try:
-                tweet = json.loads(line)
-            except ValueError:
-                continue
-            if ('retweeted_status' in tweet):
-                continue
-            else:
-                regex = [r'\bhan\b', r'\bhon\b', r'\bden\b', r'\bdet\b', r'\bdenne\b', r'\bdenna\b', r'\bhen\b']
-                tweet_counter = tweet_counter + 1
-                text = tweet['text']
-                for x in regex:
-                    found = re.findall(x, text, re.IGNORECASE)
-                    if found:
-                        dictpronoun[x[2:-2]]+=1
+        with open (filepath, 'r') as tweet:
+            for line in tweet:
+                try:
+                    tweet = json.loads(line)
+                except ValueError:
+                    continue
+                if ('retweeted_status' in tweet):
+                    continue
+                else:
+                    regex = [r'\bhan\b', r'\bhon\b', r'\bden\b', r'\bdet\b', r'\bdenne\b', r'\bdenna\b', r'\bhen\b']
+                    tweet_counter = tweet_counter + 1
+                    text = tweet['text']
+                    for x in regex:
+                        found = re.findall(x, text, re.IGNORECASE)
+                        if found:
+                            dictpronoun[x[2:-2]]+=1
 
     return (dictpronoun)
